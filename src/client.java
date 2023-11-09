54d91b70cc77fe580a5d8e6099ea2883e4b64290
Carlos <carlosdemoragil825@gmail.com>
2023-11-09 16:52:18 +0100

    Añadido ejercicio Automoviles, con clase cliente y main

---
+++ src/client.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class client {
+
+    private String id;
+    private String name;
+    private String surname;
+    private String address;
+    private ArrayList<vehicle> vehicle;
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
